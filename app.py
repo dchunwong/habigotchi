44333064231901faf72dc5ad290e001303bb6084
Linda He <bananaphone@dn0a18189c.sunet>
2014-04-05 20:12:49 -0700

    In the process of adding authentication

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request, redirect, abort
 from flask.ext.pymongo import PyMongo
 import pymongo
+
+
+#for authentications
+from functools import wraps
+from flask import request, Response
+#end authentication
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "habigotchi"
 app.config["MONGO_HOST"] = "oceanic.mongohq.com"
@@ -22,6 +28,23 @@ def submit():
 @app.route('/')
 def landing():
     return 'Hello World!'
+
+#authentication
+def check_auth(username, password):
+	"""This function is called to check if a username / password
+	   combination is valid.
+	"""
+	return username == "admin" and password == "secret"
+
+def authenticate():
+	"""Sends a 401 response that enables basic auth"""
+	return Response(
+	'Could not verify your access level for that URL.\n'
+	'You have to login with proper credentials', 401,
+	{'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+
 
 # Main Page landing.
 @app.route('/main')
