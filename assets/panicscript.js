876e0cec989fd0b75e85a2734afd9f9cdc1ab8fa
Linda He <bananaphone@dn0a18189c.sunet>
2014-04-06 10:55:16 -0700

    updated styles.css

---
+++ assets/panicscript.js
@@ -0,0 +1,36 @@
+var user, habits;
+
+
+function login(name, password){
+response = sendRequest('POST','/login',{"name":name,"password":password});
+	if (response.success){
+		user = response.user;
+		habits = response.habits;
+	}
+}
+
+function sendRequest(type, url, data){
+return JSON.parse($.ajax({
+		type: type,
+		url: url,
+		data: data,
+		async: false,
+		dataType: "json",
+	}).responseText);
+}
+
+$(document).ready( function() {
+
+	$('input[name=login-button] ').click(function (){ 
+		var name = "Hello";
+		var pw = "World";
+		login(name, pw);
+		console.log(name,", ",pw); 
+	});
+
+});
+
+
+
+
+
