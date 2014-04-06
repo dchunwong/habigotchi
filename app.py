a343ec90c2f4bcfa23d1285b028f43885c3dbfd8
Dylan Chun <d.chunwong@gmail.com>
2014-04-06 03:59:51 -0700

    API additions

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template, request, redirect, abort
+from flask import Flask, render_template, request, redirect, abort, jsonify
 from flask.ext.pymongo import PyMongo
+from ast import literal_eval
+import hashlib
 import pymongo
 
-
-#for authentications
-from functools import wraps
-from flask import request, Response
 #end authentication
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "habigotchi"
@@ -13,40 +11,34 @@ app.config["MONGO_HOST"] = "oceanic.mongohq.com"
 app.config["MONGO_PORT"] = 10008
 app.config["MONGO_USERNAME"] = "admin"
 app.config["MONGO_PASSWORD"] = "1234"
+app.debug= True
 mongo = PyMongo(app)
+
+m = hashlib.sha256
+
+def authenticate(user, pw):
+    users = mongo.db.users
+    hashed = m(user+pw).hexdigest()
+    return users.find_one({"password": hashed}) != None
 
 @app.route('/', methods=['GET', 'POST'])
 def landing():
     if request.method == 'POST':
-        user = {"Name": request.form['Name'], 
-        "number": request.form['number'], "email": request.form['email']}
+        print request
         users = mongo.db.users
-        user_id = users.insert(user)
-        return redirect('/')
+        check = users.find_one({"name":request.form['Name']})
+        if check != None:
+            return jsonify({"success": False,"message":"Name taken."})
+        else:
+            user = {"name": request.form['Name'], 
+            "number": request.form['number'],
+            "email": request.form['email'],
+            "password":m(request.form['Name']+request.form['password']).hexdigest()
+            }
+            users.insert(user)
+            return jsonify({"success": True, "message":"Sign-up successful."})
     else:
         return render_template('signup.html')
-"""
-@app.route('/')
-def landing():
-    return 'Hello World!'
-"""
-
-#authentication
-def check_auth(username, password):
-	"""This function is called to check if a username / password
-	   combination is valid.
-	"""
-	return username == "admin" and password == "secret"
-
-def authenticate():
-	"""Sends a 401 response that enables basic auth"""
-	return Response(
-	'Could not verify your access level for that URL.\n'
-	'You have to login with proper credentials', 401,
-	{'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-
-
 
 # Main Page landing.
 @app.route('/main')
@@ -56,7 +48,13 @@ def tama_page():
 # New Habits
 @app.route('/new_habit')
 def add_habit():
-    return 'Hello World!'
+    habits = mongo.db[request.form['name']]
+    check = habits.find_one({"habit":request.form["habit"]})
+    if check != None:
+        return jsonify({"success": False, "message": "Habit exists!"})
+    else: 
+        habits.insert(request.form.to_dict())
+        return jsonify({"success": True, "message": "Habit added!"})
 
 #remove a habit
 @app.route('/remove_habits')
@@ -67,6 +65,17 @@ def remove_habit():
 @app.route('/finish_habit')
 def finish_habit():
     return 'Hello World!'
+
+@app.route('/login', methods= ["POST"])
+def login():
+    recieved = literal_eval(request.data)
+    users = mongo.db.users
+    auth = authenticate(recieved['name'], recieved['password'])
+    if not auth:
+        return jsonify({"success": False, "message": "Invalid Name/Password."})
+    else:
+        user =users.find_one({"password": m(recieved['name']+recieved['password']).hexdigest()})
+        return jsonify({"success": True, "data":user})
 
 
 
